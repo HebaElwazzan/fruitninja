5cd4202f4666689844e422349cfe2bb306c9078b
Heba Elwazzan <hebaelwazan@gmail.com>
2020-04-22 19:59:14 +0200

    buffered images recommit

--- src/model/Apple.java
+++ src/model/Apple.java
@@ -1,13 +1,23 @@
 package model;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Apple extends NormalFruit{
 
 	@Override
-	public BufferedImage[] getBufferedImages() {
-		// TODO Auto-generated method stub
-		return null;
+	public BufferedImage[] getBufferedImages() throws IOException {
+		BufferedImage [] fruitImages = new BufferedImage[2];
+		fruitImages[0] = new BufferedImage(240, 240, BufferedImage.TYPE_4BYTE_ABGR);
+		fruitImages[1] = new BufferedImage(240, 240, BufferedImage.TYPE_4BYTE_ABGR);
+		File image = new File("apple.png");
+		fruitImages[0] = ImageIO.read(image);
+		image = new File("slicedApple.png");
+		fruitImages[1] = ImageIO.read(image);
+		return fruitImages;
 	}
 
 }
