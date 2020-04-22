004d4c09e9359374ce029b79128eded34a6dd412
Heba Elwazzan <hebaelwazan@gmail.com>
2020-04-22 20:07:39 +0200

    Recommitting because changes do not get pulled properly for unknown reasons

--- src/controller/SplashScreenController.java
+++ src/controller/SplashScreenController.java
@@ -10,7 +10,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
