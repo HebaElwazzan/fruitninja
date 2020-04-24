31383b0b3edb21f84c0d0b7fc0947312a2aa8fab
SH0USHA <oaboushousha99@gmail.com>
2020-04-24 17:31:36 +0200

    added game screen and transfered images into images folder in resources folder

--- src/controller/MainMenuController.java
+++ src/controller/MainMenuController.java
@@ -3,18 +3,36 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class MainMenuController implements Initializable{
 	
 	@FXML
-	AnchorPane anchorPane;
-
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		
+	Button quitButton = new Button("Quit");
+	
+	@FXML
+	private void quitButtonAction(){
+	  Stage stage = (Stage) quitButton.getScene().getWindow();
+	  stage.close();
 	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		quitButton.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				quitButtonAction();
+				
+			}
+		});
+	}
+	
 
 }
